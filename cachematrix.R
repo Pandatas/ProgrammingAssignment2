2e2e891725d3923eaa7d58a7559490166ac1bfb0
Pandatas <51525546+Pandatas@users.noreply.github.com>
2019-06-25 19:39:23 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera - John Hopkins Data Science Specialization, R-Programming week 3 assignment
 
-## Write a short comment describing this function
-
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL ## data object
+  set <- function(y) { ##set the value of the matrix
+    x <<- y ## data object
+    m <<- NULL
+  }
+  get <- function() x ##get the value of th matrix
+  setsolve <- function(solve) m <<- solve ##set the inverse of the matrix
+  getsolve <- function() m ##get the inverse of the matrix
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+## retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x, ...)  {
+  ## return the inverse of x, or calculate & return if cache is empty
+  m <- x$getsolve()
+  if(!is.null(m)) { ##check if inverse has already been calculated (and the matrix has not changed) and cached data is used
+    message("getting cached data")
+    return(m) ## return inverse from cache
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m # return inverse for changed matrix with respect to the one used in makeCacheMatrix above
 }
+
